8705a90a580e39773c561502eaaf459866fd372b
Manish Patil <patilmanishp18@gmail.com>
2024-09-29 22:16:22 +0530

    Update styles to NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,38 @@
+import { useNavigate } from "react-router-dom";
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+
+  return (
+    <section
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "var(--label-color)",
+      }}
+    >
+      <div style={{ textAlign: "center" }}>
+        <h1
+          style={{
+            color: "#fff",
+            fontSize: "6rem",
+            letterSpacing: "1.4px",
+            marginBottom: "1rem",
+          }}
+        >
+          404 - Page Not Found
+        </h1>
+        <p style={{ fontSize: "1.4rem", marginBottom: "3rem" }}>
+          Sorry, the page you are looking for does not exist.
+        </p>
+        <button className="btn-back" onClick={() => navigate("/")}>
+          Take me Back
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default NotFound;
